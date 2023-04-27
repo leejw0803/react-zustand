a4b32fd90b15520da0cd7d3fbebee5cbc986266e
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2023-04-27 13:47:56 +0900

    chore: fix ts configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import * as path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    mainFields: [],
+    alias: {
+      "@stores": path.resolve(__dirname, "src/stores"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
   plugins: [react()],
-})
+});
